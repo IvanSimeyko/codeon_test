ca9c6e49887fbca79de3c63bf95295158d0b59e2
Ivan Simeyko <iv.simeyko@gmail.com>
2015-12-01 07:15:28 +0200

    added student modify

--- students/urls_student.py
+++ students/urls_student.py
@@ -3,8 +3,7 @@ from . import views_student
 
 urlpatterns = [
     url(r'^(?P<pk>\d+)/$', views_student.StudentDetailView.as_view(), name='description_student'),
-    #url(r'^$', views.GroupListView.as_view(), name='group_list'),
-    #url(r'^edit/(?P<pk>\d+)/$', views.GroupUpdateView.as_view(), name='edit_group'),
+    url(r'^edit/(?P<pk>\d+)/$', views_student.StudentUpdateView.as_view(), name='edit_student'),
     #url(r'^add/$', views.GroupCreateView.as_view(), name='add_group'),
     #url(r'^delete/(?P<pk>\d+)/$', views.GroupDeleteView.as_view(), name='delete_group'),
     #url(r'^(?P<pk>\d+)/$', views.GroupDetailView.as_view(), name='description_group'),
--- students/views_student.py
+++ students/views_student.py
@@ -1,8 +1,12 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render, redirect
 from models import Group, Student
 from django import forms
 from django.contrib import messages
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
 
 
 class StudentDetailView(DetailView):
@@ -10,8 +14,25 @@ class StudentDetailView(DetailView):
     context_object_name = 'student'
 
 
-    #def get_context_data(self, **kwargs):
-     #   context = super(StudentDetailView, self).get_context_data(**kwargs)
-      #  group = self.get_object()
-       # context['students'] = group.student_set.all()
-        #return context
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    form_class = StudentForm
+    context_object_name = 'student'
+    template_name = 'students/group_form.html'
+    success_url = reverse_lazy('group_list')
+
+    def form_valid(self, form):
+        response = super(StudentUpdateView, self).form_valid(form)
+        messages.success(self.request, u'Данные студента %s были успешно изменены' % self.object.first_name)
+        return response
+
+    def get_context_data(self, **kwargs):
+        context = super(StudentUpdateView, self).get_context_data(**kwargs)
+        context['page_title'] = u'Редактирование данных студента'
+        return context
